23f9637895222839c10ebd111da7ab154223e76f
zoubin <zoubin04@gmail.com>
2015-11-25 18:58:44 +0800

    Source entries needed

---
+++ test/reduce.js
@@ -0,0 +1,29 @@
+var test = require('tape')
+var Reduce = require('../')
+var path = require('path')
+var fixtures = path.resolve.bind(path, __dirname)
+
+test('single-bundle', function(t) {
+  var r = Reduce({
+    entries: '**/*.js',
+    basedir: fixtures('src', 'single-bundle'),
+    js: {
+      dest: fixtures('build', 'js'),
+    },
+    css: {
+      reduceOpts: {
+        factor: 'common.js',
+      },
+      dest: [
+        fixtures('build', 'css'),
+        null,
+        {
+          maxSize: 0,
+          name: '[name].[hash]',
+          assetOutFolder: fixtures('build', 'assets'),
+        },
+      ],
+    },
+  })
+})
+
