3126ca7341e173240887cefb498779064c601ba8
JulesWang <w.jq0722@gmail.com>
2016-03-24 14:28:18 +0800

    add param basedir

--- test/input-map.js
+++ test/input-map.js
@@ -3,13 +3,17 @@ var test = require('tap').test
 var path = require('path')
 var fs = require('fs')
 var fixtures = path.resolve.bind(path, __dirname, 'fixtures')
+var src = fixtures.bind(null, 'src', 'reduce')
 
 test('inputMap', function(t) {
   var opts = require(fixtures('reduce.config'))
-  opts.map = fixtures('deps.js')
+  opts.map = {
+    basedir: src(),
+    file: fixtures('deps.js'),
+  }
   return reduce.bundle(opts).then(function() {
-    var data = fs.readFileSync(opts.map)
-    fs.unlinkSync(opts.map)
+    var data = fs.readFileSync(opts.map.file)
+    fs.unlinkSync(opts.map.file)
     var o = JSON.parse(data)
     t.same(
       o['page/hi/index.css'],
